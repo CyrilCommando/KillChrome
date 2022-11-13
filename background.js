91e6c6bc8d8a8153814168648ad4ed488e0076f8
CyrilCommando <44797228+CyrilCommando@users.noreply.github.com>
2022-11-13 13:27:57 -0600

    Add files via upload

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
+chrome.runtime.onStartup.addListener(function(){
+    chrome.tabs.create(
+        {
+            url: chrome.runtime.getURL("nav.html")
+        }
+    )
+})
+
 chrome.tabs.create(
     {
         url: chrome.runtime.getURL("nav.html")
